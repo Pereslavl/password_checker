c91609c0e5b53bb150ee03b6be483011cc89cbff
Sergey Kryukov <cydkom@gmail.com>
2018-12-22 22:25:36 +0300

    MASTER_PASSWORD

--- password_checker.py
+++ password_checker.py
@@ -1,9 +1,9 @@
 import sys
 
-
- = input("Please enter the super secret password:")
+MASTER_PASSWORD = "opensesame"
+password = input("Please enter the super secret password:")
 attempt_count = 1
-while password != "opensesame":
+while password != MASTER_PASSWORD:
     if attempt_count > 3:
         sys.exit("Too many invalid password attempts")
     password = input("Invalid password, try again: ")
